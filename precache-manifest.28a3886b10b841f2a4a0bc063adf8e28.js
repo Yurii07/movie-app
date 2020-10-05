e2aa83e7dac65a4d8ffc95410c9a8e2c6bc539cf
Yurii Adamenko <skyey07@gmail.com>
2020-10-05 23:37:21 +0300

    Updates

---
+++ precache-manifest.28a3886b10b841f2a4a0bc063adf8e28.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "821ef0d239c3aea6baebbd950f18b896",
+    "url": "/movie-app/index.html"
+  },
+  {
+    "revision": "0bea9bc8eb0210406dec",
+    "url": "/movie-app/static/css/main.35a3330d.chunk.css"
+  },
+  {
+    "revision": "a3583996acdbf430fa89",
+    "url": "/movie-app/static/js/2.39ba52e6.chunk.js"
+  },
+  {
+    "revision": "0bea9bc8eb0210406dec",
+    "url": "/movie-app/static/js/main.c7abf449.chunk.js"
+  },
+  {
+    "revision": "5ccce974657b8a913657",
+    "url": "/movie-app/static/js/runtime-main.1f64301b.js"
+  }
+]);
--- src/APIKey.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    APIKey: '2952064a'
-}
